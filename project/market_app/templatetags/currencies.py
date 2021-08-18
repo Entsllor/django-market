fe41d923a663d2f864cfc8fce7217d3f4c9fe315
Lysov Viacheslav <slava90100@yandex.com>
2021-08-18 21:50:43 +0500

    feat(market_app): add currencies functional

---
+++ project/market_app/templatetags/currencies.py
@@ -0,0 +1,10 @@
+from django import template
+from ..services import get_currency, exchange_to
+register = template.Library()
+
+
+@register.filter
+def currency_l10n(money_in_default_currency, language):
+    currency = get_currency(language)
+    exchanged_amount = exchange_to(currency.code, money_in_default_currency)
+    return f'{exchanged_amount}{currency.sym}'
--- project/market_app/tests/test_services.py
+++ project/market_app/tests/test_services.py
@@ -1,8 +1,15 @@
+import copy
 from decimal import Decimal
+
+from django.test import SimpleTestCase
 
 from .base_case import TestBaseWithFilledCatalogue, BaseMarketTestCase, assert_difference
 from ..models import ShoppingAccount, ShoppingReceipt, Coupon
-from ..services import top_up_balance, make_purchase, withdraw_money, NotEnoughMoneyError
+from ..services import (
+    top_up_balance, make_purchase, withdraw_money,
+    NotEnoughMoneyError, get_currency, exchange_to,
+    DEFAULT_CURRENCY, currencies, language_currency, Currency
+)
 
 
 class ChangeBalanceTest(BaseMarketTestCase):
@@ -139,3 +146,42 @@ class CouponTest(TestBaseWithFilledCatalogue):
         make_purchase(self.shopping_account)
         self.assertIsNone(self.shopping_account.activated_coupon)
         self.assertFalse(self.shopping_account.coupon_set.filter(pk=activated_coupon.pk).exists())
+
+
+class CurrencyTest(SimpleTestCase):
+    def setUp(self) -> None:
+        self.currencies_backup = copy.deepcopy(currencies)
+
+    def test_get_currency(self):
+        for language_code, currency_code in language_currency.items():
+            currency = get_currency(language_code)
+            self.assertEqual(currency.code, currency_code)
+
+    def test_return_default_if_unexpected_language(self):
+        self.assertNotEqual('TEST_CODE', DEFAULT_CURRENCY)
+        currency = get_currency('TEST_CODE')
+        self.assertEqual(currency.code, DEFAULT_CURRENCY)
+
+    def test_exchange_to(self):
+        amount_to_exchange = 100
+        rate_of_test_currency = Decimal('2.33')
+        currencies['test'] = Currency(code='TEST', sym='T', rate=rate_of_test_currency)
+        exchanged_amount = exchange_to('test', amount_to_exchange)
+        self.assertEqual(exchanged_amount, rate_of_test_currency * amount_to_exchange)
+
+    def test_pass_exchanging_if_codes_are_equal(self):
+        amount_to_exchange = 100
+        rate_of_test_currency = Decimal('2.33')
+        currencies['test'] = Currency(code='TEST', sym='T', rate=rate_of_test_currency)
+        exchanged_amount = exchange_to('test', amount_to_exchange, 'test')
+        self.assertEqual(exchanged_amount, amount_to_exchange)
+
+    def test_exchange_to_default(self):
+        amount_to_exchange = 100
+        rate_of_test_currency = Decimal('5')
+        currencies['test'] = Currency(code='TEST', sym='T', rate=rate_of_test_currency)
+        exchanged_amount = exchange_to(DEFAULT_CURRENCY, amount_to_exchange, _from='test')
+        self.assertEqual(exchanged_amount, 20)
+
+    def tearDown(self) -> None:
+        currencies = self.currencies_backup
