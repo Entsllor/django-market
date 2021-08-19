e0430d32f071ef2e89780af83aff2f9caad794de
Lysov Viacheslav <slava90100@yandex.com>
2021-08-19 15:16:17 +0500

    fix(services): convert str to decimal during currency exchanging even if exchange-currency-code equals from-currency-code

--- project/market_app/services.py
+++ project/market_app/services.py
@@ -38,10 +38,10 @@ def get_currency(language_str: str):
 
 
 def exchange_to(currency_code, amount, _from=DEFAULT_CURRENCY):
-    if currency_code == _from:
-        return amount
     if isinstance(amount, str):
         amount = Decimal(amount)
+    if currency_code == _from:
+        return amount
     exchange_rate = currencies[currency_code].rate / currencies[_from].rate
     exchanged_amount = (amount * exchange_rate).quantize(Decimal('1.00'))
     return exchanged_amount
