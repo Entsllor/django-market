56dbec36b2e68c1a80f1f19de3275f72df6accaa
Lysov Viacheslav <slava90100@yandex.com>
2021-08-22 12:03:09 +0500

    feat(currencies): display default currency if a current user language is unknown

--- project/currencies/templatetags/currencies.py
+++ project/currencies/templatetags/currencies.py
@@ -2,6 +2,7 @@ import logging
 
 from django import template
 from django.conf import settings
+from currencies.models import Currency
 
 from currencies.services import get_currency_by_language, _exchange, _get_exchange_rate
 
@@ -18,12 +19,17 @@ def to_local_currency(amount):
 
 @register.simple_tag
 def get_currency_l10n_filter(language):
-    currency = get_currency_by_language(language)
-    rate = _get_exchange_rate(currency.code)
+    try:
+        currency = get_currency_by_language(language)
+        rate = _get_exchange_rate(currency.code)
+        currency_sym = currency.sym
+    except Currency.DoesNotExist:
+        rate = 1
+        currency_sym = settings.CURRENCIES_SYMBOLS.get(settings.DEFAULT_CURRENCY, '')
 
     @register.filter
     def to_local_currency(amount):
         exchanged_amount = _exchange(amount, rate)
-        return f'{exchanged_amount}{currency.sym}'
+        return f'{exchanged_amount}{currency_sym}'
 
     return to_local_currency
