885b059492dfef917378744cceb16b34e83f25e3
Lysov Viacheslav <slava90100@yandex.com>
2021-09-19 23:15:54 +0500

    feat(product_page): display products if user cart is empty

--- project/market_app/templatetags/market_app_blocks.py
+++ project/market_app/templatetags/market_app_blocks.py
@@ -1,5 +1,7 @@
 from django import template
 
+from market_app.models import Product
+
 register = template.Library()
 
 
@@ -11,3 +13,11 @@ def side_nav(context):
 @register.inclusion_tag('market_app/include/search.html')
 def product_search_form():
     return
+
+
+@register.inclusion_tag('market_app/include/catalogue.html', takes_context=True)
+def products_catalogue(context, count=36, ordering="discount_percent"):
+    products = Product.objects.select_related('productimage').filter(
+        available=True).order_by(ordering)[:count]
+    context['products'] = products
+    return context
