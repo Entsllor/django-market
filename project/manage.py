c3a79dee512878c25252fc07fd551d40286cc7c3
Entsllor <slava90100@yandex.com>
2022-01-05 21:14:40 +0500

    feat: add settings module env aliases

--- project/manage.py
+++ project/manage.py
@@ -5,13 +5,27 @@ import sys
 
 from project.settings.base_settings import env
 
+PRODUCTION = 'project.settings.production'
+DEVELOPMENT = 'project.settings.development'
+TESTING = 'project.settings.testing'
+
 
 def main():
     """Run administrative tasks."""
     if '--settings' not in ' '.join(sys.argv):
-        env('DJANGO_SETTINGS_MODULE', default='project.settings.production')
-    if 'test' in sys.argv and '--settings' not in ' '.join(sys.argv):
-        sys.argv.append('--settings=project.settings.testing')
+        settings_module = env('DJANGO_SETTINGS_MODULE', default='').lower()
+        # if you need to run tests with another module add '--settings SETTINGS_MODULE_NAME' argument
+        if 'test' in sys.argv:
+            settings_module = TESTING
+        # aliases
+        elif settings_module in ('prod', 'production'):
+            settings_module = PRODUCTION
+        elif settings_module in ('dev', 'development'):
+            settings_module = DEVELOPMENT
+        elif settings_module in ('test', 'testing'):
+            settings_module = TESTING
+        sys.argv.append(f"--settings={settings_module}")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
