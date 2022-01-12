f663e028477460d8acc350c60d2024f762081d7c
Entsllor <slava90100@yandex.com>
2022-01-12 20:54:15 +0500

    fix(docker-compose): wait for db loading before run app

--- project/docker-entrypoint.sh
+++ project/docker-entrypoint.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+
+# Wait for db
+if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ]
+then
+  echo "Wait for Database - ${DB_HOST}:${DB_PORT}"
+  timeout 20 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 0.1; done' "${DB_HOST}" "${DB_PORT}"
+  echo "Database - ${DB_HOST}:${DB_PORT} is loaded"
+fi
+
 ./manage.py configure_market
 uwsgi --ini uwsgi.ini
