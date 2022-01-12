ef3dcf317b054411160ae780c59d503505ecdf15
Entsllor <slava90100@yandex.com>
2022-01-13 00:24:41 +0500

    feat(docker-compose): print if the app failed to connect to a database

--- project/docker-entrypoint.sh
+++ project/docker-entrypoint.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 # Wait for db
-if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ]
-then
+if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ]; then
   echo "Wait for Database - ${DB_HOST}:${DB_PORT}"
-  timeout 20 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 0.1; done' "${DB_HOST}" "${DB_PORT}"
-  echo "Database - ${DB_HOST}:${DB_PORT} is loaded"
+  if timeout 20 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 0.1; done' "${DB_HOST}" "${DB_PORT}"
+  then
+    echo "Database - ${DB_HOST}:${DB_PORT} is loaded"
+  else
+    echo "Failed to connect to db - ${DB_HOST}:${DB_PORT}"
+  fi
 fi
 
 ./manage.py configure_market
